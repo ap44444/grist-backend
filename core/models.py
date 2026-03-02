c65787c24286c216d85db121274839816e29489e
Mahima Senarathna <mahimasenarathna@MacBookPro.mshome.net>
2026-03-02 14:42:45 +0530

    Removed redundant RegularUserProfile, re-linked ForeignKeys, and mapped UserProfile to CustomUser

--- core/models.py
+++ core/models.py
@@ -2,23 +2,12 @@ from django.db import models
 from django.contrib.auth.models import AbstractUser
 from django.core.validators import MinValueValidator
 from pgvector.django import VectorField
-from django.contrib.auth.models import User
+
 
 # --- MODULE 1: USERS ---
 class CustomUser(AbstractUser):
     is_dietician = models.BooleanField(default=False)
     profile_picture = models.URLField(blank=True, null=True)
-
-
-class RegularUserProfile(models.Model):
-    user = models.OneToOneField(CustomUser, on_delete=models.CASCADE, primary_key=True, related_name='regular_profile')
-    birth_date = models.DateField(null=True, blank=True)
-    gender = models.CharField(max_length=10, choices=[('M', 'Male'), ('F', 'Female'), ('O', 'Other')])
-    activity_level = models.CharField(max_length=20, choices=[('sedentary', 'Sedentary'), ('active', 'Active')])
-    current_weight_kg = models.DecimalField(max_digits=5, decimal_places=2, null=True)
-    height_cm = models.DecimalField(max_digits=5, decimal_places=2, null=True)
-    dietary_goal = models.CharField(max_length=50, default="Maintenance")
-
 
 class DieticianProfile(models.Model):
     user = models.OneToOneField(CustomUser, on_delete=models.CASCADE, primary_key=True,
@@ -71,7 +60,7 @@ class RecipeIngredient(models.Model):
 
 # --- MODULE 3: MEAL PLANS ---
 class WeeklyPlan(models.Model):
-    user = models.ForeignKey(RegularUserProfile, on_delete=models.CASCADE)
+    user = models.ForeignKey(UserProfile, on_delete=models.CASCADE)
     start_date = models.DateField()
     end_date = models.DateField()
     total_cost_lkr = models.DecimalField(max_digits=10, decimal_places=2, default=0)
@@ -91,7 +80,7 @@ class MealSlot(models.Model):
 
 # --- MODULE 4: EXTRAS ---
 class ConsultationRequest(models.Model):
-    user = models.ForeignKey(RegularUserProfile, on_delete=models.CASCADE)
+    user = models.ForeignKey(UserProfile, on_delete=models.CASCADE)
     dietician = models.ForeignKey(DieticianProfile, on_delete=models.CASCADE)
     status = models.CharField(max_length=20, default='pending')
     created_at = models.DateTimeField(auto_now_add=True)
@@ -121,7 +110,7 @@ class PriceUpdate(models.Model):
 
 class UserProfile(models.Model):
 
-    user = models.OneToOneField(User, on_delete=models.CASCADE, related_name='profile')
+    user = models.OneToOneField(CustomUser, on_delete=models.CASCADE, related_name='profile')
     # Demographics
     GENDER_CHOICES = [('Male', 'Male'), ('Female', 'Female')]
     gender = models.CharField(max_length=10, choices=GENDER_CHOICES)
