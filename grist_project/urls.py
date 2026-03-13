5c4886cc240ae9abab35c4648938e99d58aed7ba
Mahima Senarathna <mahima.4f@gmail.com>
2026-03-13 12:07:34 +0530

    Added JWT token refresh endpoint to maintain persistent user sessions

--- grist_project/urls.py
+++ grist_project/urls.py
@@ -1,5 +1,6 @@
 from django.contrib import admin
 from django.urls import path
+from rest_framework_simplejwt.views import TokenRefreshView
 from core import views as core_views
 from rest_framework_simplejwt.views import TokenObtainPairView
 from core.views import RegisterView
@@ -22,4 +23,6 @@ urlpatterns = [
     path('cart/add/', views.add_cart_item, name='add_cart_item'),            # CREATE
     path('cart/item/<int:item_id>/update/', views.update_cart_item, name='update_item'), # UPDATE
     path('cart/item/<int:item_id>/delete/', views.delete_cart_item, name='delete_item'), # DELETE
+    # The Token Refresh Endpoint
+    path('api/token/refresh/', TokenRefreshView.as_view(), name='token_refresh'),
 ]
