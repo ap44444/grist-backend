b6adeae2f23b01849a96897a5ba53f407e403fbe
Maheen <chipe.guy6000@gmail.com>
2026-03-20 10:18:29 +0530

    settings update for profile picture upload

--- grist_project/settings.py
+++ grist_project/settings.py
@@ -142,3 +142,16 @@ SIMPLE_JWT = {
     'ROTATE_REFRESH_TOKENS': False,
     'BLACKLIST_AFTER_ROTATION': True,
 }
+
+#profile picture
+import os
+import cloudinary
+import cloudinary.uploader
+import cloudinary.api
+
+# Cloudinary Configuration
+cloudinary.config(
+  cloud_name = os.getenv('CLOUDINARY_CLOUD_NAME'),
+  api_key = os.getenv('CLOUDINARY_API_KEY'),
+  api_secret = os.getenv('CLOUDINARY_API_SECRET')
+)
